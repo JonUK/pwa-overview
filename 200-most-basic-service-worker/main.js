2c610218c67a3033e2ffbde7818bd5b154c345d8
JonUK <jonuk2018@jjwebdesign.co.uk>
2018-08-15 21:51:04 +0100

    Fixed typo

--- 200-most-basic-service-worker/main.js
+++ 200-most-basic-service-worker/main.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   console.log('Congratulations! You have a modern browser which supports service worker.');
 
   // The scope is the path where the sw.js exists.
-  // The service worker only can run and work withing the location.
+  // The service worker only can run and work within the location.
   // TODO: Show the scope of the service worker in Chrome DevTools.
   navigator.serviceWorker.register('sw.js')
     .then((registration) => {
