c74fc18505055ca523554619c72af6d7aa7c013a
JonUK <jonuk2018@jjwebdesign.co.uk>
2018-08-17 00:43:30 +0100

    More tweaks

--- 200-most-basic-service-worker/sw.js
+++ 200-most-basic-service-worker/sw.js
@@ -2,3 +2,7 @@
 // NOTE: We have no access to the window object here and no access to the page
 
 console.log('From sw.js.', 'Hello from the service worker. The JavaScript was parsed OK.');
+
+// TODO: Add install & activate event handlers
+// TODO: Show how new service worker is installed but in "waiting to be active" state
+// TODO: Show scope and clients in the Chrome DevTools
