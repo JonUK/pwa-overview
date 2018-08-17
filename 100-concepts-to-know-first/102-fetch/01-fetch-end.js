d098c2a0358179d629a04b4193af453d4c5b1c48
JonUK <jonuk2018@jjwebdesign.co.uk>
2018-08-18 00:21:51 +0100

    Tweaks to the fetch example and database

--- 100-concepts-to-know-first/102-fetch/01-fetch-end.js
+++ 100-concepts-to-know-first/102-fetch/01-fetch-end.js
@@ -1,5 +1,5 @@
 
-fetch('https://api.github.com/users/JonUK/repos')
+fetch('http://localhost:3000/todos')
   .then(response => {
     return response.json();
   })
@@ -12,4 +12,12 @@ fetch('https://api.github.com/users/JonUK/repos')
 // The body of ReadableStream is considered "consumable" meaning it can only be read once.
 // If a ReadableStream needs to be consumed more than once, create a copy using the clone().
 
+const newTodo = { name: 'Eat a pot noodle' };
 
+fetch('http://localhost:3000/todos', {
+  method: 'POST',
+  body: JSON.stringify(newTodo),
+  headers: {
+    'content-type': 'application/json'
+  }
+})
--- 100-concepts-to-know-first/102-fetch/01-fetch-start.js
+++ 100-concepts-to-know-first/102-fetch/01-fetch-start.js
@@ -1,5 +1,16 @@
 
-// TODO: fetch https://api.github.com/users/JonUK/repos and get the JSON from the response
+// TODO: Make a GET fetch to http://localhost:3000/todos and get the JSON from the response
 
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Syntax
 // https://developer.mozilla.org/en-US/docs/Web/API/Response#Methods
+
+
+
+// TODO: Make a POST fetch to http://localhost:3000/todos to create a new todo item
+// TODO: Ensure method, body and headers > content-type options are set
+const newTodo = { name: 'Eat a pot noodle' };
+
+
+
+
+// NOTE: The endpoint must be setup to with the appropriate CORS headers when spanning a different origin
