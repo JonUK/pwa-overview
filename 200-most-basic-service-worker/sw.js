44c452e38a7e528d5b30fec3e3e8dbe3626e697f
JonUK <jonuk2018@jjwebdesign.co.uk>
2018-08-18 00:34:44 +0100

    Added additional comment

--- 200-most-basic-service-worker/sw.js
+++ 200-most-basic-service-worker/sw.js
@@ -6,3 +6,4 @@ console.log('From sw.js.', 'Hello from the service worker. The JavaScript was pa
 // TODO: Add install & activate event handlers (at the same time)
 // TODO: Show how new service worker is installed but in "waiting to be active" state
 // TODO: Show scope and clients in the Chrome DevTools
+// Note: When working in a service worker, we refer to the worker itself by the keyword self
