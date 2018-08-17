43ad5412e827569ecb47b94d8dfdab3e7fa63806
JonUK <jonuk2018@jjwebdesign.co.uk>
2018-08-17 15:03:44 +0100

    Added some files to act as notes

---
+++ 300-notes/javascript.js
@@ -0,0 +1,10 @@
+
+// JavaScript element selectors
+let commentElement = document.getElementById('comment');
+let pElements = document.getElementsByTagName('p');
+let requiredElements = document.getElementsByClassName('required');
+
+
+
+
+
